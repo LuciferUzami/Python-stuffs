2cafe5e9b51c1f4a9f70794f4cefb4559789056b
LuciferUzami <praveentechci2023@gmail.com>
2024-09-16 16:55:14 +0530

    Set time on send morning Quotes

--- Email Sender/main.py
+++ Email Sender/main.py
@@ -1,25 +1,45 @@
-# import lib
+# import libs
 from smtplib import SMTP
 from email.mime.text import MIMEText
+from random import choice
 import password_manager
-#
-# #
-password = "Your password"  # App generating password
-subject = "Hello Praveen"
-body = "Connection Successfully"
-msg = MIMEText(body, "plain")
-msg['From'] = "Your email"
-msg["To"] = "Reciver eamil"
-msg["Subject"] = subject
-#
-# #
-with SMTP(host="smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user= "Your email", password=password)
-    connection.sendmail(from_addr="Your email",
-                        to_addrs="Reciver eamil",
-                        msg=msg.as_string())
-print("Send Successfully")
+import morning_quotes as quotes
+import datetime as dt
 
+# Set email sending time
+send_time = dt.datetime.now().strftime("%I : %M %p")
+# Remove the zero
+if send_time.startswith("0"):
+    send_time = send_time[1:]
 
-i
+# Get random daily quotes
+if send_time == "6 : 00 AM":
+    daily_quotes = choice(quotes.morning_quotes)
+    # #
+    password = password_manager.password
+    subject = "Hello Praveen Kumar Good Morning"
+    body = f"""
+    Hi Praveen,
+    
+    Good morning! ☀️ I hope today brings you a fresh start, new opportunities, and the energy to achieve all your goals! 💪✨
+    
+    Here’s a little boost of motivation to kickstart your day:
+    
+    {daily_quotes}
+    
+    """
+    msg = MIMEText(body, "plain")
+    msg['From'] = "Your email"
+    msg["To"] = "Reciver email"
+    msg["Subject"] = subject
+    #
+    # #
+    with SMTP(host="smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user= "Your mail", password=password)
+        connection.sendmail(from_addr="Your mail",
+                            to_addrs="Reciever mail",
+                            msg=msg.as_string())
+    print("Send Successfully")
+else:
+    print("Not have a time yet")
