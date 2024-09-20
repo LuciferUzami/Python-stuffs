ac214f227f4125160217215b9de41861e3ed252b
LuciferUzami <praveentechci2024@gmail.com>
2024-09-20 17:01:17 +0530

    On process

--- GUI/Turtle/hirstPainting.py
+++ GUI/Turtle/hirstPainting.py
@@ -16,7 +16,26 @@ with Image.open(image_path) as img:
 img_colors = colorgram.extract("./Media/Images/resize_painting.jpg", 100)
 colors = [ color.rgb for color in img_colors]
 
+#  Tom position
+starting_x = -300
+starting_y = -250
+tom.penup()
+moveing = True
+tom.setposition(starting_x, starting_y)
+while moveing:
+    if starting_x == 300:
+        starting_y += 30
+        tom.setposition(starting_x, starting_y)
+    else:
+        tom.pendown()
+        tom.dot(20, "red")
+        tom.penup()
+        tom.forward(30)
+        starting_x += 30
 
+
+#  Screen out
+screen.exitonclick()
 
 
 
