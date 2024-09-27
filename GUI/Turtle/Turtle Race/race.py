edd33065f056cbd039434c606d748575b8075f83
LuciferUzami <praveentechci2023@gmail.com>
2024-09-27 10:24:42 +0530

    Updating

--- GUI/Turtle/Turtle Race/race.py
+++ GUI/Turtle/Turtle Race/race.py
@@ -1,1 +1,23 @@
-print("Hello")
+from random import choice
+from turtle import Turtle, Screen
+
+# Player
+colors = ["red", "green", "blue", '''Yellow''']
+for num in range(4):
+    tom_variants = Turtle()
+    tom_variants.shape("turtle")
+    tom_variants.color(colors[num])
+
+#
+screen = Screen()
+
+# Get user input
+user_choice = screen.textinput("Turtle Race Game", "Which color turtle you will choose?:")
+print(user_choice)
+
+# Set color
+
+
+
+#
+screen.exitonclick()
